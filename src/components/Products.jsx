c1247ee5c8c4bb365fe5f0e93e2cd1dbd754c3ac
Zaid <zaidshaikh0024@gmail.com>
2024-09-09 01:38:03 +0530

    UI: add Projects components

---
+++ src/components/Products.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import Product from "./Product";
+
+function Products() {
+  var products = [
+    {
+      title: "arqitel",
+      description:
+        "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Obcaecati,vero laudantium temporibus quod atque dolorum aliquam earum!",
+      live: false,
+      case: false,
+    },
+    {
+      title: "TTR",
+      description:
+        "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Obcaecati,vero laudantium temporibus quod atque dolorum aliquam earum!",
+      live: true,
+      case: false,
+    },
+    {
+      title: "YIR 2022",
+      description:
+        "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Obcaecati,vero laudantium temporibus quod atque dolorum aliquam earum!",
+      live: true,
+      case: true,
+    },
+    {
+      title: "yahoo",
+      description:
+        "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Obcaecati,vero laudantium temporibus quod atque dolorum aliquam earum!",
+      live: true,
+      case: false,
+    },
+  ];
+
+  return (
+    <div>
+      {products.map((elem, index) => (
+        <Product val={elem} />
+      ))}
+    </div>
+  );
+}
+
+export default Products;
